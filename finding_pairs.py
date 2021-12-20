7b3d7983b58165bd2bb56704d3dc3b78ba334e03
Harika Talari <87809170+HarikaTalari@users.noreply.github.com>
2021-12-20 17:51:04 +0530

    Create finding_pairs.py

---
+++ finding_pairs.py
@@ -0,0 +1,10 @@
+def find_pairs_of_numbers(num_list,n):
+    count=0
+    for i in num_list:
+        if n-i in num_list:
+            count+=1
+    return (count//2)
+
+num_list=[1, 2, 4, 5, 6]
+n=6
+print(find_pairs_of_numbers(num_list,n))
